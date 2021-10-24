80b85682a362f10f77420cae8d2260993f8d414b
Ko.Yelie <ko.yelie@gmail.com>
2021-10-25 02:01:47 +0900

    rename

--- examples/shaders/fitCover.glsl
+++ examples/shaders/fitCover.glsl
@@ -1,4 +1,4 @@
-vec2 adjustRatio(vec2 uv, vec2 inputResolution, vec2 outputResolution) {
+vec2 fitCover(vec2 uv, vec2 inputResolution, vec2 outputResolution) {
   vec2 ratio = vec2(
     min((outputResolution.x / outputResolution.y) / (inputResolution.x / inputResolution.y), 1.),
     min((outputResolution.y / outputResolution.x) / (inputResolution.y / inputResolution.x), 1.)
@@ -6,4 +6,4 @@ vec2 adjustRatio(vec2 uv, vec2 inputResolution, vec2 outputResolution) {
   return uv * ratio + (1. - ratio) * 0.5;
 }
 
-#pragma glslify: export(adjustRatio)
+#pragma glslify: export(fitCover)
