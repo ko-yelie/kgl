eaa4e9ea9d9da874c8216e0713bd1a6dd38ae5a2
Ko.Yelie <ko.yelie@gmail.com>
2022-06-22 00:21:31 +0900

    add example: fit DOM, fit Multiple DOM

---
+++ examples/kgl/dom-multiple/index.frag
@@ -0,0 +1,22 @@
+precision highp float;
+
+uniform vec2 uResolutionShape;
+uniform sampler2D uImage;
+uniform vec2 uImageResolution;
+uniform float uScrollDiff;
+
+varying vec2 vUv;
+
+#pragma glslify: PI = require(../../shaders/PI.glsl)
+#pragma glslify: fitContain = require(../../shaders/fitContain.glsl)
+#pragma glslify: discardOutOfRangeUv = require(../../shaders/discardOutOfRangeUv.glsl)
+
+void main() {
+  vec2 uv = vUv;
+  uv.y = 1. - uv.y;
+  uv = fitContain(uv, uImageResolution, uResolutionShape);
+  uv.y += sin(uv.x * PI) * uScrollDiff;
+  discardOutOfRangeUv(uv);
+
+  gl_FragColor = texture2D(uImage, uv);
+}
--- examples/kgl/dom/index.js
+++ examples/kgl/dom/index.js
@@ -3,91 +3,43 @@ import fragmentShader from './index.frag'
 import { onLoadImage } from '../../utils.js'
 
 const kgl = new Kgl({
-  canvas: 'canvas',
+  canvas: '.canvas',
   hasCamera: true,
   isFullSize: true,
 })
 
-function createImage(img) {
-  const { width, height } = img.getBoundingClientRect()
+async function main() {
+  /**
+   * program
+   */
+  const img = await onLoadImage(document.querySelector('.image'))
 
   const program = kgl.createProgram({
     shape: 'plane',
     fragmentShader,
     uniforms: {
       uImage: img,
-      uImageResolution: [width, height],
-      uScrollDiff: 0,
+      uTime: 0,
     },
     isTransparent: true,
     isAutoAdd: true,
-    data: {
-      img,
-    },
   })
-
-  return program
-}
-
-async function main() {
-  /**
-   * program
-   */
-  const groupCloud = kgl.createGroup({
-    isAutoAdd: true,
-  })
-
-  document.querySelectorAll('.image').forEach(async (img) => {
-    await onLoadImage(img)
-    const program = createImage(img)
-    groupCloud.add(program)
-    resizeProgram(program)
-  })
-
-  /**
-   * scroll
-   */
-  let scrollPrev = 0
-  let scrollDiff = 0
-  let scrollDiffSmooth = 0
-  let timerId
-  function scroll() {
-    groupCloud.y = window.scrollY
-
-    scrollDiff = window.scrollY - scrollPrev
-    scrollPrev = window.scrollY
-
-    if (timerId) {
-      clearTimeout(timerId)
-    }
-    timerId = setTimeout(() => {
-      scrollDiff = 0
-    }, 300)
-  }
-  scroll()
-  window.addEventListener('scroll', scroll)
 
   /**
    * resize
    */
-  function resizeProgram(program) {
-    const { width, height, top, left } =
-      program.data.img.getBoundingClientRect()
+  function resize() {
+    kgl.resize()
 
-    program.scale = width
+    const { width, height, top, left } = img.getBoundingClientRect()
+
+    program.scale2d = [width, height]
     program.x = left - (window.innerWidth * 0.5 - width * 0.5)
     program.y = -(
       top +
       window.scrollY -
       (window.innerHeight * 0.5 - height * 0.5)
     )
-  }
-  function resize() {
-    kgl.resize()
-
-    groupCloud.forEachProgram((program) => {
-      resizeProgram(program)
-    })
   }
   resize()
   window.addEventListener('resize', resize)
@@ -96,14 +48,9 @@ async function main() {
    * tick
    */
   function tick(time) {
-    scrollDiffSmooth += (scrollDiff - scrollDiffSmooth) * 0.05
-    if (Math.abs(scrollDiff - scrollDiffSmooth) < 0.01) {
-      scrollDiffSmooth = scrollDiff
-    }
+    time *= 0.001
 
-    groupCloud.forEachProgram((program) => {
-      program.uniforms.uScrollDiff = scrollDiffSmooth * 0.003
-    })
+    program.uniforms.uTime = time
 
     kgl.draw()
 
